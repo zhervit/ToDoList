417141393d49e8a8b4b8e84bf61d6e3dad3d67fc
Victor_Zherdev <zhervit@gmail.com>
2018-09-25 18:07:41 +0300

    initial commit

---
+++ ToDoList/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using React.AspNet;
+
+namespace ToDoList
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		//public void ConfigureServices(IServiceCollection services)
+		//{
+		//	services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		//}
+
+		public IServiceProvider ConfigureServices(IServiceCollection services)
+		{
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			services.AddReact();
+			services.AddMvc();
+
+			return services.BuildServiceProvider();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			//if (env.IsDevelopment())
+			//{
+			//	app.UseDeveloperExceptionPage();
+			//}
+			//else
+			//{
+			//	app.UseHsts();
+			//}
+
+			//app.UseHttpsRedirection();
+			//app.UseMvc();
+
+			app.UseReact(config => { });
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+			app.UseMvc();
+		}
+	}
+}
